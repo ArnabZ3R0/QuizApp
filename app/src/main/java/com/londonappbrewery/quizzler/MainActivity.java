614dbc04e56c56ff5561018231e57a8efe427ec3
Arnab Basu <arbose95@gmail.com>
2017-12-02 11:32:33 +0530

    initial app

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,39 +1,120 @@
 package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     // TODO: Declare constants here
 
 
+
     // TODO: Declare member variables here:
 
+    Button mTrueButton;
+    Button mFalseButton;
+    TextView mQuestionTextView;
+    int mIndex = 0;
+    int mScore = 0;
+    int questionId;
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+   private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+           new TrueFalse(R.string.question_2, true),
+           new TrueFalse(R.string.question_3, true),
+           new TrueFalse(R.string.question_4, true),
+           new TrueFalse(R.string.question_5, true),
+           new TrueFalse(R.string.question_6, false),
+           new TrueFalse(R.string.question_7, true),
+           new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+           new TrueFalse(R.string.question_10, true),
+           new TrueFalse(R.string.question_11, false),
+           new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
+
+    final int PROGRESS_AMOUNT = (int) Math.ceil(100.0/mQuestionBank.length);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mScoreTextView = (TextView)findViewById(R.id.score);
+
+        mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
 
 
+        questionId = mQuestionBank[mIndex].getmQuestionID();
+
+        mQuestionTextView.setText(questionId);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View v){
+                        checkAnswer(false);
+                        updateQuestion();
+                    }
+                });
+
+
+    }
+
+    public void updateQuestion(){
+        mIndex =  (mIndex+1) % 13;
+
+        if(mIndex == 0)
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("GAME OVER!");
+            alert.setCancelable(false);
+            alert.setMessage("You Scored:" + mScore + " points!");
+            alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+
+        questionId = mQuestionBank[mIndex].getmQuestionID();
+        mQuestionTextView.setText(questionId);
+        mScoreTextView.setText(mScore + "/" + mQuestionBank.length);
+    }
+
+    public void checkAnswer(boolean userInput){
+        boolean correctAnswer = mQuestionBank[mIndex].isManswer();
+
+        if(correctAnswer == userInput){
+            Toast.makeText(getApplicationContext(),"Nailed it!",Toast.LENGTH_SHORT).show();
+            mProgressBar.incrementProgressBy(PROGRESS_AMOUNT);
+            mScore += 1;
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"You are a dumbfuck",Toast.LENGTH_SHORT).show();
+        }
     }
 }
